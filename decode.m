4e71154b45fabe89c2060e3d8eb6c57c2deadf0a
drid <drid@stadar.org>
2017-05-20 20:09:23 +0300

    Initial commit

---
+++ decode.m
@@ -0,0 +1,72 @@
+pkg load signal
+clear all
+
+_PLAYBACK = true;
+_PLOT = false;
+
+%arg_list = argv()
+%filename = arg_list{1}
+filename = '';
+
+audio=wavread(filename);
+
+w1=medfilt1(abs(audio), 1000);
+
+level_threshold = mean(w1)
+
+if _PLOT
+  plot(w1)
+  hold
+  plot([0,length(w1)], [level_threshold, level_threshold])
+endif
+
+w1(w1 > level_threshold) =1;
+w1(w1 < level_threshold) =0;
+
+w1 = medfilt1(w1, 100);
+w1 = not(w1);
+
+ind = find(diff(w1))+1;
+len=diff([1; ind]);
+
+symbol_threshold = max(len(2:2:end))/2;
+letter_threshold = min(len(1:2:end))*2;
+
+morseC='';
+packet='';
+
+for idx = w1(1)+2:2:size(len)-1
+  if len(idx) > symbol_threshold
+    morseC = strcat(morseC, "-");
+   else
+    morseC = strcat(morseC, ".");
+   endif
+
+  if len(idx+1) > letter_threshold
+    packet = strcat(packet, morse2char(morseC));
+    %printf ("%s %s\n", morse2char(morseC), morseC);
+    morseC='';
+  endif
+endfor
+
+printf("Packet: %s\n", packet)
+
+%% Playback
+if _PLAYBACK
+% carrier generation
+  bps = 16;       % bits per sample
+  sps = 32000;     % sample rate [samples/s]
+  freq = 600;       % frequency of the tone [Hz]
+  nsecs = length(w1)/sps;      % number of seconds of the audio file
+
+  nsamples = sps*nsecs;
+
+  time = linspace(0, nsecs, nsamples);
+  carrier = sin(time*2*pi*freq)';
+
+  %ws=w1(1:size(carrier));
+
+  wc = carrier(1:length(w1)) .* w1;
+
+  sound(wc, sps);
+endif
---
+++ morse2char.m
@@ -0,0 +1,113 @@
+function character = morse2char(mc)
+  character = '';
+  switch mc
+    case ".-"
+      character = 'A';
+      return
+    case "-..."
+      character = 'B';
+      return
+    case "-.-."
+      character = 'C';
+      return
+    case "-.."
+      character = 'D';
+      return
+    case "."
+      character = 'E';
+      return
+    case "..-.."
+      character = 'F';
+      return
+    case "--."
+      character = 'G';
+      return
+    case "...."
+      character = 'H';
+      return
+    case ".."
+      character = 'I';
+      return
+    case ".---"
+      character = 'J';
+      return
+    case "-.-"
+      character = 'K';
+      return
+    case ".-.."
+      character = 'L';
+      return
+    case "--"
+      character = 'M';
+      return
+    case "-."
+      character = 'N';
+      return
+    case "---"
+      character = 'O';
+      return
+    case ".--."
+      character = 'P';
+      return
+    case "--.-"
+      character = 'Q';
+      return
+    case ".-."
+      character = 'R';
+      return
+    case "..."
+      character = 'S';
+      return
+    case "-"
+      character = 'T';
+      return
+    case "..-"
+      character = 'U';
+      return
+    case "...-"
+      character = 'V';
+      return
+    case ".--"
+      character = 'W';
+      return
+    case "-..-"
+      character = 'X';
+      return
+    case "-.--"
+      character = 'Y';
+      return
+    case "--.."
+      character = 'Z';
+      return
+    case ".----"
+      character = '1';
+      return
+    case "..---"
+      character = '2';
+      return
+    case "...--"
+      character = '3';
+      return
+    case "....-"
+      character = '4';
+      return
+    case "....."
+      character = '5';
+      return
+    case "-...."
+      character = '6';
+      return
+    case "--..."
+      character = '7';
+      return
+    case "---.."
+      character = '8';
+      return
+    case "----."
+      character = '9';
+      return
+    case "-----"
+      character = '0';
+      return
+  endswitch
+endfunction
